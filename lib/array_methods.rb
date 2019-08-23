dc2e6710d9face736b49823ed808e319ebdca3f6
Keginold Philip <keginoldp@hotmail.com>
2019-08-23 15:27:43 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  a = [1,2,3,4,5]
+  a.include?(3)   # => true
+  a.include?(9)
 end
 
 def find_max_value(array)
   # Add your solution here
+   max_number = array[0]
 end
 
 def find_min_value(array)
   # Add your solution here
+   min_number = array[0]
 end
